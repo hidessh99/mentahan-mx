c44f0676f2c0459491abc606f5a746297ae0fa92
arismuslimin <93474156+arismuslimin@users.noreply.github.com>
2022-08-26 13:42:01 +0700

    Update install-tools.sh

--- install-tools.sh
+++ install-tools.sh
@@ -14,7 +14,7 @@ LIGHT='\033[0;37m'
 MYIP=$(wget -qO- ipinfo.io/ip);
 # ==================================================
 # Link Hosting Kalian
-akbarvpn="raw.githubusercontent.com/arismuslimin/profil/"
+akbarvpn="raw.githubusercontent.com/arismuslimin/xraymulti/main/data/"
 
 #
 cd
