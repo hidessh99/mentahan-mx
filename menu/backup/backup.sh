774dd6d4e5bc6f7173470c84c3c8f34ef6d67f27
arismuslimin <93474156+arismuslimin@users.noreply.github.com>
2022-08-26 14:11:14 +0700

    Update backup.sh

--- menu/backup/backup.sh
+++ menu/backup/backup.sh
@@ -32,11 +32,19 @@ cp /etc/passwd backup/
 cp /etc/group backup/
 cp /etc/shadow backup/
 cp /etc/gshadow backup/
+cp -r /etc/wireguard backup/wireguard
 cp /etc/ppp/chap-secrets backup/chap-secrets
+cp /etc/ipsec.d/passwd backup/passwd1
+cp /etc/shadowsocks-libev/akun.conf backup/ss.conf
 cp -r /var/lib/crot/ backup/crot
+cp -r /home/sstp backup/sstp
 cp -r /etc/xray backup/xray
-cp -r /root/domain backup/domain
+cp -r /root/nsdomain backup/nsdomain
+cp -r /etc/slowdns backup/slowdns
+cp -r /etc/trojan-go backup/trojan-go
+cp -r /usr/local/shadowsocksr/ backup/shadowsocksr
 cp -r /home/vps/public_html backup/public_html
+cd /root
 cd /root
 zip -r $IP-$date.zip backup > /dev/null 2>&1
 rclone copy /root/$IP-$date.zip dr:backup/
