790345eee33d4b5be4da66a6aea9a3e63374aeb3
arismuslimin <93474156+arismuslimin@users.noreply.github.com>
2022-08-27 05:03:47 +0700

    Update install-tools.sh

--- install-tools.sh
+++ install-tools.sh
@@ -13,10 +13,6 @@ LIGHT='\033[0;37m'
 # Getting
 MYIP=$(wget -qO- ipinfo.io/ip);
 # ==================================================
-# Link Hosting Kalian
-akbarvpn="raw.githubusercontent.com/arismuslimin/xraymulti/"
-
-#
 cd
 
 # Edit file /etc/systemd/system/rc-local.service
@@ -62,7 +58,7 @@ chmod +x /etc/rc.local
 # enable rc local
 systemctl enable rc-local
 systemctl start rc-local.service
-#
+
 # set time GMT +7
 ln -fs /usr/share/zoneinfo/Asia/Jakarta /etc/localtime
 
@@ -116,29 +112,8 @@ apt install libssl1.0-dev -y
 apt install dos2unix -y
 apt install speedtest-cli -y
 
-#
 # install
 apt-get --reinstall --fix-missing install -y bzip2 gzip coreutils wget screen rsyslog iftop htop net-tools zip unzip wget net-tools curl nano sed screen gnupg gnupg1 bc apt-transport-https build-essential dirmngr libxml-parser-perl neofetch git lsof
 echo "clear" >> .profile
 echo "neofetch" >> .profile
-
-# install webserver
-apt -y install nginx php php-fpm php-cli php-mysql libxml-parser-perl
-rm /etc/nginx/sites-enabled/default
-rm /etc/nginx/sites-available/default
-curl https://${akbarvpn}/nginx.conf > /etc/nginx/nginx.conf
-curl https://${akbarvpn}/vps.conf > /etc/nginx/conf.d/vps.conf
-sed -i 's/listen = \/var\/run\/php-fpm.sock/listen = 127.0.0.1:9000/g' /etc/php/fpm/pool.d/www.conf
-useradd -m vps;
-mkdir -p /home/vps/public_html
-echo "<?php phpinfo() ?>" > /home/vps/public_html/info.php
-chown -R www-data:www-data /home/vps/public_html
-chmod -R g+rw /home/vps/public_html
-cd /home/vps/public_html
-wget -O /home/vps/public_html/index.html "https://${akbarvpn}/index.html"
-/etc/init.d/nginx restart
-cd
-cd
-chown -R www-data:www-data /home/vps/public_html
-/etc/init.d/nginx restart
 cd
