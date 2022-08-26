de0ba6276f9541ec1698ddc641b82be3f1ae5aca
arismuslimin <93474156+arismuslimin@users.noreply.github.com>
2022-08-27 05:06:40 +0700

    Update updatedll.sh

--- menu/updatedll.sh
+++ menu/updatedll.sh
@@ -10,25 +10,25 @@ cd /usr/bin
 rm -rf xmenu
 rm -rf updatedll
 rm -r updatedll
+
 # download
-#
 cd /usr/bin
-wget -O xmenu "https://raw.githubusercontent.com/arismuslimin/xraymulti/main/menu/xmenu.sh"
+wget -O add-akun "https://raw.githubusercontent.com/arismuslimin/xraymulti/main/menu/backup/set-br.sh"
 wget -O add-akun "https://raw.githubusercontent.com/arismuslimin/xraymulti/main/menu/add-akun.sh"
 wget -O updatedll "https://raw.githubusercontent.com/arismuslimin/xraymulti/main/menu/updatedll.sh"
 wget -O add-akun "https://raw.githubusercontent.com/arismuslimin/xraymulti/main/menu/add-akun.sh"
 wget -O delete-akun "https://raw.githubusercontent.com/arismuslimin/xraymulti/main/menu/delete-akun.sh"
 wget -O certv2ray "https://raw.githubusercontent.com/arismuslimin/xraymulti/main/menu/certv2ray.sh"
 wget -O restart-xray "https://raw.githubusercontent.com/arismuslimin/xraymulti/main/menu/restart-xray.sh"
-wget -O xmenu "https://raw.githubusercontent.com/arismuslimin/xraymulti/main/menu/xmenu.sh"
 wget -O auto-pointing "https://raw.githubusercontent.comarismuslimin/xraymulti/main/menu/auto-pointing.sh"
 wget -O cek-port "https://raw.githubusercontent.com/arismuslimin/xraymulti/main/menu/cek-port.sh"
 wget -O xmenu "https://raw.githubusercontent.com/arismuslimin/xraymulti/main/menu/xmenu.sh"
 
-#
+# permision
 chmod +x /usr/bin/updatedll
 chmod +x /usr/bin/xmenu
 chmod +x xmenu
+chmod +x set-br
 chmod +x add-akun
 chmod +x delete-akun
 chmod +x updatedll
