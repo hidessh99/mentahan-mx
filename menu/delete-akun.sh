9f96e54c6cd1b92480ed5c2b7ff24da03ae1a59e
arismuslimin <93474156+arismuslimin@users.noreply.github.com>
2022-08-28 06:42:49 +0700

    Update delete-akun.sh

--- menu/delete-akun.sh
+++ menu/delete-akun.sh
@@ -31,11 +31,13 @@ NUMBER_OF_CLIENTS=$(grep -c -E "^### " "/etc/xray/config.json")
     echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 	read -rp "Input Username : " user
     if [ -z $user ]; then
+    
     xmenu
     else
     exp=$(grep -wE "^### $user" "/etc/xray/config.json" | cut -d ' ' -f 3 | sort | uniq)
     sed -i "/^### $user $exp/,/^},{/d" /etc/xray/config.json
-    systemctl restart xray > /dev/null 2>&1
+    
+    systemctl restart xray
     clear
     echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
     echo " V2RAY Account Deleted Successfully"
@@ -45,6 +47,6 @@ NUMBER_OF_CLIENTS=$(grep -c -E "^### " "/etc/xray/config.json")
     echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
     echo ""
     read -n 1 -s -r -p "Press any key to back on menu"
-    
     xmenu
+    
     fi
