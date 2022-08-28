d5e7275d60f6ea071a524443f580e2953fad881b
arismuslimin <93474156+arismuslimin@users.noreply.github.com>
2022-08-29 01:43:18 +0700

    Create menu-backup.sh

---
+++ menu/menu-backup.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+clear
+echo -e "\e[36m╒════════════════════════════════════════════╕\033[0m"
+echo -e " \E[0;41;36m                 INFO SERVER                \E[0m"
+echo -e "\e[36m╘════════════════════════════════════════════╛\033[0m"
+uphours=`uptime -p | awk '{print $2,$3}' | cut -d , -f1`
+upminutes=`uptime -p | awk '{print $4,$5}' | cut -d , -f1`
+uptimecek=`uptime -p | awk '{print $6,$7}' | cut -d , -f1`
+cekup=`uptime -p | grep -ow "day"`
+IPVPS=$(curl -s ipinfo.io/ip )
+ISPVPS=$( curl -s ipinfo.io/org )
+#clear
+if [ "$cekup" = "day" ]; then
+echo -e "System Uptime   :  $uphours $upminutes $uptimecek"
+else
+echo -e "System Uptime   :  $uphours $upminutes"
+fi
+echo -e "IP-VPS          :  $IPVPS"
+echo -e "ISP-VPS         :  $ISPVPS"
+echo "╔═════════════════════════════════════════════════════════════════╗"
+echo "║                       ┃ Script By Akcell ┃                      ║" 
+echo "╚═════════════════════════════════════════════════════════════════╝"
+echo "╔═════════════════════════════════════════════════════════════════╗"
+echo "║                           085730123218                          ║" 
+echo "╚═════════════════════════════════════════════════════════════════╝"
+echo "╔═════════════════════════════════════════════════════════════════╗"
+echo "║                     ┃ XRAY XMENU BACKUP ┃                       ║" 
+echo "╚═════════════════════════════════════════════════════════════════╝"  
+echo "║ 1. Backup Manual Via Email                                      ║"
+echo "║ 2. Backup Auto Via Email                                        ║"
+echo "║ 3. Restore Backup Via link                                      ║"
+echo "║ 4. Ubah Email Penerima                                          ║"
+echo "║ 5. Tes Kirim Email                                              ║"
+echo "║ 6. Menu Utama                                                   ║"
+echo "║ 7. Exit                                                         ║"
+echo "╚═════════════════════════════════════════════════════════════════╝" 
+read -p "     Select From Options [1-69 or x] :  " xmenu
+case $xmenu in 
+1)
+backup
+;;
+2)
+autobackup
+;;
+3)
+restore
+;;
+4)
+add-email
+;;
+5)
+send-email
+;;
+6)
+xmenu
+;;
+7)
+exit
+;;
+echo "Input The Correct Number !"
+;;
+esac
