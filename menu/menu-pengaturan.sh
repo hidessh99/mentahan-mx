1144d959f0054bb59df9d8abd75e26df5ae0074d
arismuslimin <93474156+arismuslimin@users.noreply.github.com>
2022-08-29 01:37:10 +0700

    Create menu-pengaturan.sh

---
+++ menu/menu-pengaturan.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+clear
+echo -e "\e[36m╒════════════════════════════════════════════╕\033[0m"
+echo -e " \E[0;41;36m                 INFO SERVER                \E[0m"
+echo -e "\e[36m╘════════════════════════════════════════════╛\033[0m"
+uphours=`uptime -p | awk '{print $2,$3}' | cut -d , -f1`
+upminutes=`uptime -p | awk '{print $4,$5}' | cut -d , -f1`
+uptimecek=`uptime -p | awk '{print $6,$7}' | cut -d , -f1`
+cekup=`uptime -p | grep -ow "day"`
+IPVPS=$(curl -s ipinfo.io/ip )
+ISPVPS=$( curl -s ipinfo.io/org )
+#clear
+if [ "$cekup" = "day" ]; then
+echo -e "System Uptime   :  $uphours $upminutes $uptimecek"
+else
+echo -e "System Uptime   :  $uphours $upminutes"
+fi
+echo -e "IP-VPS          :  $IPVPS"
+echo -e "ISP-VPS         :  $ISPVPS"
+echo "╔═════════════════════════════════════════════════════════════════╗"
+echo "║                       ┃ Script By Akcell ┃                      ║" 
+echo "╚═════════════════════════════════════════════════════════════════╝"
+echo "╔═════════════════════════════════════════════════════════════════╗"
+echo "║                           085730123218                          ║" 
+echo "╚═════════════════════════════════════════════════════════════════╝"
+echo "╔═════════════════════════════════════════════════════════════════╗"
+echo "║                     ┃ XRAY XMENU PENGATURAN ┃                   ║" 
+echo "╚═════════════════════════════════════════════════════════════════╝"  
+echo "║ 1. Pengaturan                                                   ║"
+echo "║ 2. Pengaturan 2                                                 ║"
+echo "║ 3.                                                              ║"
+echo "║ 4.                                                              ║"
+echo "║ 5.                                                              ║"
+echo "║ 6. Menu Utama                                                   ║"
+echo "║ 7. Exit                                                         ║"
+echo "╚═════════════════════════════════════════════════════════════════╝" 
+read -p "     Select From Options [1-69 or x] :  " xmenu
+case $xmenu in 
+1)
+pengaturan
+;;
+2)
+pengaturan2
+;;
+3)
+pengaturan3
+;;
+4)
+pengaturan4
+;;
+5)
+pengaturan5
+;;
+6)
+xmenu
+;;
+7)
+exit
+;;
+echo "Input The Correct Number !"
+;;
+esac
