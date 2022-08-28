5ebec1c6c41b75d464bfddb957f3245a2c5e6d77
arismuslimin <93474156+arismuslimin@users.noreply.github.com>
2022-08-29 00:13:48 +0700

    Update delete-akun.sh

--- menu/delete-akun.sh
+++ menu/delete-akun.sh
@@ -35,7 +35,7 @@ NUMBER_OF_CLIENTS=$(grep -c -E "^# " "/etc/xray/config.json")
 	echo " Press CTRL+C to return"
 	echo " ==============================="
 	echo "     No  Expired   User"
-	grep -E "^#t " "/etc/xray/config.json" | cut -d ' ' -f 2-3 | nl -s ') '
+	grep -E "^# " "/etc/xray/config.json" | cut -d ' ' -f 2-3 | nl -s ') '
 	until [[ ${CLIENT_NUMBER} -ge 1 && ${CLIENT_NUMBER} -le ${NUMBER_OF_CLIENTS} ]]; do
 		if [[ ${CLIENT_NUMBER} == '1' ]]; then
 			read -rp "Select one client [1]: " CLIENT_NUMBER
@@ -43,10 +43,10 @@ NUMBER_OF_CLIENTS=$(grep -c -E "^# " "/etc/xray/config.json")
 			read -rp "Select one client [1-${NUMBER_OF_CLIENTS}]: " CLIENT_NUMBER
 		fi
 	done
-user=$(grep -E "^#t " "/etc/xray/config.json" | cut -d ' ' -f 2 | sed -n "${CLIENT_NUMBER}"p)
-exp=$(grep -E "^#t " "/etc/xray/config.json" | cut -d ' ' -f 3 | sed -n "${CLIENT_NUMBER}"p)
-sed -i "/^#t $user $exp/,/^},{/d" /etc/xray/config.json
-sed -i "/^#t $user $exp/,/^},{/d" /etc/xray/config.json
+user=$(grep -E "^# " "/etc/xray/config.json" | cut -d ' ' -f 2 | sed -n "${CLIENT_NUMBER}"p)
+exp=$(grep -E "^# " "/etc/xray/config.json" | cut -d ' ' -f 3 | sed -n "${CLIENT_NUMBER}"p)
+sed -i "/^# $user $exp/,/^},{/d" /etc/xray/config.json
+sed -i "/^# $user $exp/,/^},{/d" /etc/xray/config.json
 systemctl restart xray.service
 service cron restart
 clear
@@ -57,4 +57,4 @@ echo "==============================="
 echo "Username  : $user"
 echo "Expired   : $exp"
 echo "==============================="
-echo "Script Mod By SL"
+echo "Script Mod akcell"
