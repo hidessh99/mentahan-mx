af8713c3e40b418fc2ab5f370c1a24b58a8e059d
arismuslimin <93474156+arismuslimin@users.noreply.github.com>
2022-08-29 13:15:03 +0700

    Create menu-update.sh

---
+++ menu/menu-update.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+clear
+echo -e "\e[36m╒════════════════════════════════════════════╕\033[0m"
+echo -e " \E[0;41;36m                 INFO SERVER                \E[0m"
+echo -e "\e[36m╘════════════════════════════════════════════╛\033[0m"
+uphours=`uptime -p | awk '{print $2,$3}' | cut -d , -f1`
+upminutes=`uptime -p | awk '{print $4,$5}' | cut -d , -f1`
+uptimecek=`uptime -p | awk '{print $6,$7}' | cut -d , -f1`
+cekup=`uptime -p | grep -ow "day"`
+IPVPS=$(curl -s ipinfo.io/ip )
+ISPVPS=$( curl -s ipinfo.io/org )
+#clear
+if [ "$cekup" = "day" ]; then
+echo -e "System Uptime   :  $uphours $upminutes $uptimecek"
+else
+echo -e "System Uptime   :  $uphours $upminutes"
+fi
+echo -e "IP-VPS          :  $IPVPS"
+echo -e "ISP-VPS         :  $ISPVPS"
+echo "╔═════════════════════════════════════════════════════════════════╗"
+echo "║                       ┃ Script By Akcell ┃                      ║" 
+echo "╚═════════════════════════════════════════════════════════════════╝"
+echo "╔═════════════════════════════════════════════════════════════════╗"
+echo "║                           085730123218                          ║" 
+echo "╚═════════════════════════════════════════════════════════════════╝"
+echo "╔═════════════════════════════════════════════════════════════════╗"
+echo "║                     ┃ XRAY XMENU UPDATE ALL ┃                   ║" 
+echo "╚═════════════════════════════════════════════════════════════════╝"  
+echo "║ 1. Update Sript                                                 ║"
+echo "║ 2. Update Menu dll                                              ║"
+echo "║ 3. Update Menu B&R                                              ║"
+echo "║ 4. Update Lainnya                                               ║"
+echo "║ 5. Update info                                                  ║"
+echo "║ 6. Menu Utama                                                   ║"
+echo "║ 7. Exit                                                         ║"
+echo "╚═════════════════════════════════════════════════════════════════╝" 
+read -p "     Select From Options [1-69 or x] :  " xmenu
+case $xmenu in 
+1)
+update-sr
+;;
+2)
+update-menu
+;;
+3)
+update-br
+;;
+4)
+update-lain
+;;
+5)
+update-info
+;;
+6)
+xmenu
+;;
+7)
+exit
+;;
+echo "Input The Correct Number !"
+;;
+esac
