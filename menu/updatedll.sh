992c2e32156b5445570825ba08ec0061be13ecdf
arismuslimin <93474156+arismuslimin@users.noreply.github.com>
2022-08-29 14:07:24 +0700

    Update updatedll.sh

--- menu/updatedll.sh
+++ menu/updatedll.sh
@@ -17,18 +17,13 @@ akcell="raw.githubusercontent.com/arismuslimin/xraymulti/main/menu"
 # download
 cd /usr/bin
 wget -O xmenu "https://${akcell}/xmenu.sh"
-wget -O add-akun "https://${akcell}/add-akun.sh"
-wget -O delete-akun "https://${akcell}/delete-akun.sh"
+wget -O add-akun "https://${akcell}/buat/add-akun.sh"
+wget -O delete-akun "https://${akcell}/hapus/delete-akun.sh"
 wget -O updatedll "https://${akcell}/updatedll.sh"
-wget -O certv2ray "https://${akcell}/certv2ray.sh"
-wget -O restart-xray "https://${akcell}/restart-xray.sh"
-wget -O auto-pointing "https://${akcell}/auto-pointing.sh"
-wget -O cek-port "https://${akcell}/cek-port.sh"
-wget -O autobackup "https://${akcell}/autobackup.sh"
-wget -O backup "https://${akcell}/backup.sh"
-wget -O restore "https://${akcell}/restore.sh"
-wget -O strt "https://${akcell}/strt.sh"
-wget -O limitspeed "https://${akcell}/limitspeed.sh"
+wget -O certv2ray "https://${akcell}/fix/certv2ray.sh"
+wget -O restart-xray "https://${akcell}/fix/restart-xray.sh"
+wget -O auto-pointing "https://${akcell}/domain/auto-pointing.sh"
+wget -O cek-port "https://${akcell}/pengaturan/cek-port.sh"
 
 # permision
 chmod +x /usr/bin/updatedll
@@ -41,10 +36,5 @@ chmod +x certv2ray
 chmod +x restart-xray
 chmod +x auto-pointing
 chmod +x cek-port
-chmod +x autobackup
-chmod +x backup
-chmod +x restore
-chmod +x strt
-chmod +x limitspeed
 
 cd
