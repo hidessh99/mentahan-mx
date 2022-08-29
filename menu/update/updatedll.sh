023021cd78b1e15ffe4b316d406b4f50be4c9de9
arismuslimin <93474156+arismuslimin@users.noreply.github.com>
2022-08-29 13:37:46 +0700

    Update updatedll.sh

--- menu/update/updatedll.sh
+++ menu/update/updatedll.sh
@@ -11,24 +11,29 @@ rm -rf xmenu
 rm -rf updatedll
 rm -r updatedll
 
+#link hosting
+akcell="raw.githubusercontent.com/arismuslimin/xraymulti/main/menu"
+
 # download
 cd /usr/bin
-wget -O xmenu "https://raw.githubusercontent.com/arismuslimin/xraymulti/main/menu/xmenu.sh"
-wget -O set-br "https://raw.githubusercontent.com/arismuslimin/xraymulti/main/menu/backup/set-br.sh"
-wget -O add-akun "https://raw.githubusercontent.com/arismuslimin/xraymulti/main/menu/add-akun.sh"
-wget -O delete-akun "https://raw.githubusercontent.com/arismuslimin/xraymulti/main/menu/delete-akun.sh"
-wget -O updatedll "https://raw.githubusercontent.com/arismuslimin/xraymulti/main/menu/updatedll.sh"
-wget -O certv2ray "https://raw.githubusercontent.com/arismuslimin/xraymulti/main/menu/certv2ray.sh"
-wget -O restart-xray "https://raw.githubusercontent.com/arismuslimin/xraymulti/main/menu/restart-xray.sh"
-wget -O auto-pointing "https://raw.githubusercontent.comarismuslimin/xraymulti/main/menu/auto-pointing.sh"
-wget -O cek-port "https://raw.githubusercontent.com/arismuslimin/xraymulti/main/menu/cek-port.sh"
-
+wget -O xmenu "https://${akcell}/xmenu.sh"
+wget -O add-akun "https://${akcell}/add-akun.sh"
+wget -O delete-akun "https://${akcell}/delete-akun.sh"
+wget -O updatedll "https://${akcell}/updatedll.sh"
+wget -O certv2ray "https://${akcell}/certv2ray.sh"
+wget -O restart-xray "https://${akcell}/restart-xray.sh"
+wget -O auto-pointing "https://${akcell}/auto-pointing.sh"
+wget -O cek-port "https://${akcell}/cek-port.sh"
+wget -O autobackup "https://${akcell}/autobackup.sh"
+wget -O backup "https://${akcell}/backup.sh"
+wget -O restore "https://${akcell}/restore.sh"
+wget -O strt "https://${akcell}/strt.sh"
+wget -O limitspeed "https://${akcell}/limitspeed.sh"
 
 # permision
 chmod +x /usr/bin/updatedll
 chmod +x /usr/bin/xmenu
 chmod +x xmenu
-chmod +x set-br
 chmod +x add-akun
 chmod +x delete-akun
 chmod +x updatedll
@@ -36,5 +41,10 @@ chmod +x certv2ray
 chmod +x restart-xray
 chmod +x auto-pointing
 chmod +x cek-port
+chmod +x autobackup
+chmod +x backup
+chmod +x restore
+chmod +x strt
+chmod +x limitspeed
 
 cd
