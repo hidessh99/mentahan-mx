10b8553fd3d203efcfee898fb8b33aaf50ad7600
arismuslimin <93474156+arismuslimin@users.noreply.github.com>
2022-08-31 05:10:56 +0700

    Update add-vless.sh

--- menu/buat/add-vless.sh
+++ menu/buat/add-vless.sh
@@ -29,8 +29,8 @@ domain=$(cat /etc/xray/domain)
 else
 domain=$IP
 fi
-tls="$(cat ~/log-install.txt | grep -w "Vless TLS" | cut -d: -f2|sed 's/ //g')"
-nontls="$(cat ~/log-install.txt | grep -w "Vless None TLS" | cut -d: -f2|sed 's/ //g')"
+tls="$(cat ~/log-install.txt | grep -w "VLESS WS TLS" | cut -d: -f2|sed 's/ //g')"
+nontls="$(cat ~/log-install.txt | grep -w "VLESS WS HTTP" | cut -d: -f2|sed 's/ //g')"
 until [[ $user =~ ^[a-zA-Z0-9_]+$ && ${CLIENT_EXISTS} == '0' ]]; do
 		read -rp "Username : " -e user
 		CLIENT_EXISTS=$(grep -w $user /etc/xray/config.json | wc -l)
@@ -49,7 +49,7 @@ sed -i '/#vless$/a\## '"$user $exp"'\
 },{"id": "'""$uuid""'","email": "'""$user""'"' /etc/xray/config.json
 sed -i '/#vlessgrpc$/a\## '"$user $exp"'\
 },{"id": "'""$uuid""'","email": "'""$user""'"' /etc/xray/config.json
-vlesslinkws=""vless://${uuid}@${domain}:80?path=/xrayws&security=none&encryption=none&host=${domain}&type=ws#${user}"
+vlesslinkws="vless://${uuid}@${domain}:80?path=/xrayws&security=none&encryption=none&host=${domain}&type=ws#${user}"
 vlesslinkwstls="vless://${uuid}@${domain}:443?path=/xrayws&security=tls&encryption=none&host=${domain}&type=ws&sni=${domain}#${user}"
 vlesslinkgrpc="vless://${uuid}@${domain}:443?mode=gun&security=tls&encryption=none&type=grpc&serviceName=vless-grpc&sni=${domain}#${user}"
 systemctl restart xray.service
@@ -64,8 +64,8 @@ echo -e "Port TLS    : $tls"
 echo -e "Port No TLS : $nontls"
 echo -e "User ID     : ${uuid}"
 echo -e "Encryption  : none"
-echo -e "Network     : ws"
-echo -e "Path        : /vless/"
+echo -e "Network     : ws/grpc"
+echo -e "Path        : /xrayws/vless-grpc"
 echo -e "Created     : $hariini"
 echo -e "Expired     : $exp"
 echo -e "========================="
@@ -73,4 +73,4 @@ echo -e "Link TLS    : ${vlesslinkwstls}"
 echo -e "========================="
 echo -e "Link No TLS : ${vlesslinkgrpc}"
 echo -e "========================="
-echo -e "Script Mod By SL"
+echo -e "AKCELL XRAY MULTI VLESS"
