a78ed1d385bbc0a4f0711c76709339e6327c758c
arismuslimin <93474156+arismuslimin@users.noreply.github.com>
2022-08-30 12:31:33 +0700

    Update set-br.sh

--- menu/set-br.sh
+++ menu/set-br.sh
@@ -38,7 +38,7 @@ port 587
 auth on
 user akcellulerofficial@gmail.com
 from akcellulerofficial@gmail.com
-password pkqqrgtlemyegpgl
+password alvwqpqwjqqhzlso
 logfile ~/.msmtp.log
 EOF
 chown -R www-data:www-data /etc/msmtprc
