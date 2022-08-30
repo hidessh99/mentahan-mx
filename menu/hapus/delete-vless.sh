d6601490e3d46452c711a67d4dd0af6e04539be0
arismuslimin <93474156+arismuslimin@users.noreply.github.com>
2022-08-31 05:59:59 +0700

    Update delete-vless.sh

--- menu/hapus/delete-vless.sh
+++ menu/hapus/delete-vless.sh
@@ -45,13 +45,14 @@ NUMBER_OF_CLIENTS=$(grep -c -E "^## " "/etc/xray/config.json")
 	done
 user=$(grep -E "^## " "/etc/xray/config.json" | cut -d ' ' -f 2 | sed -n "${CLIENT_NUMBER}"p)
 exp=$(grep -E "^## " "/etc/xray/config.json" | cut -d ' ' -f 3 | sed -n "${CLIENT_NUMBER}"p)
-sed -i "/^## $user $exp/,/^},{/d" /etc/xray/config.json
-sed -i "/^## $user $exp/,/^},{/d" /etc/xray/config.json
+hariini=$(grep -E "^## " "/etc/xray/config.json" | cut -d ' ' -f 4 | sed -n "${CLIENT_NUMBER}"p)
+uuid=$(grep -E "^## " "/etc/xray/config.json" | cut -d ' ' -f 5 | sed -n "${CLIENT_NUMBER}"p)
+sed -i "/^## $user $exp $hariini $uuid/,/^},{/d" /etc/xray/config.json
 systemctl restart xray.service
 clear
 echo ""
 echo "==============================="
-echo "  XRAYS/Vmess Account Deleted  "
+echo "${NC}${RED}  XRAYS/Vmess Account Deleted ${NC}"
 echo "==============================="
 echo "Username  : $user"
 echo "Expired   : $exp"
