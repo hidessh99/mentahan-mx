bd8e611f4becb08db3b2d6e6f84fa4b4f13234a6
arismuslimin <93474156+arismuslimin@users.noreply.github.com>
2022-08-31 09:21:31 +0700

    Update cek-akun.sh

--- menu/cek/cek-akun.sh
+++ menu/cek/cek-akun.sh
@@ -32,6 +32,9 @@ tls="$(cat ~/log-install.txt | grep -w "VMESS WS TLS" | cut -d: -f2|sed 's/ //g'
 nontls="$(cat ~/log-install.txt | grep -w "VMESS WS HTTP" | cut -d: -f2|sed 's/ //g')"
 clear
 NUMBER_OF_CLIENTS=$(grep -c -E "^# " "/etc/xray/config.json")
+NUMBER_OF_CLIENTS=$(grep -c -E "^## " "/etc/xray/config.json")
+NUMBER_OF_CLIENTS=$(grep -c -E "^### " "/etc/xray/config.json")
+NUMBER_OF_CLIENTS=$(grep -c -E "^#### " "/etc/xray/config.json")
 	if [[ ${NUMBER_OF_CLIENTS} == '0' ]]; then
 		echo ""
 		echo "You have no existing clients!"
@@ -45,9 +48,6 @@ NUMBER_OF_CLIENTS=$(grep -c -E "^# " "/etc/xray/config.json")
 	echo " ==============================="
 	echo "     No    User   Expired"
 	grep -E "^#-^# " "/etc/xray/config.json" | cut -d ' ' -f 2-3 | nl -s ') '
-	grep -E "^#-^## " "/etc/xray/config.json" | cut -d ' ' -f 2-3 | nl -s ') '
-	grep -E "^#-^### " "/etc/xray/config.json" | cut -d ' ' -f 2-3 | nl -s ') '
-	grep -E "^#-^#### " "/etc/xray/config.json" | cut -d ' ' -f 2-3 | nl -s ') '
 	until [[ ${CLIENT_NUMBER} -ge 1 && ${CLIENT_NUMBER} -le ${NUMBER_OF_CLIENTS} ]]; do
 		if [[ ${CLIENT_NUMBER} == '1' ]]; then
 			read -rp "Select one client [1]: " CLIENT_NUMBER
