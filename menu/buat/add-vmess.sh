568bc26963b6fe2bd7c03690e26d0ed5becd542e
arismuslimin <93474156+arismuslimin@users.noreply.github.com>
2022-08-31 22:59:10 +0700

    Update add-vmess.sh

--- menu/buat/add-vmess.sh
+++ menu/buat/add-vmess.sh
@@ -44,12 +44,11 @@ uuid=$(cat /proc/sys/kernel/random/uuid)
 read -p "Expired (Days) : " masaaktif
 hariini=`date -d "0 days" +"%Y-%m-%d"`
 exp=`date -d "$masaaktif days" +"%Y-%m-%d"`
-v="vmess"
-w="wstls"
-g="grpc
-sed -i '/#vmess$/a\### '"$user $exp $hariini $v$w $uuid"'\
+ws="vmess"
+gr="vmessgrpc"
+sed -i '/#$ws$/a\### '"$user $exp $hariini $sw $uuid"'\
 },{"id": "'""$uuid""'","alterId": '"0"',"email": "'""$user""'"' /etc/xray/config.json
-sed -i '/#vmessgrpc$/a\### '"$user $exp $hariini $v$g $uuid"'\
+sed -i '/#$gr$/a\### '"$user $exp $hariini $gr $uuid"'\
 },{"id": "'""$uuid""'","alterId": '"0"',"email": "'""$user""'"' /etc/xray/config.json
 cat>/etc/xray/vmess-$user-ws.json<<EOF
       {
