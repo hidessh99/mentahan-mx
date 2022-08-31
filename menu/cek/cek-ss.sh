8b4929f8c6ae61b0353c1498ff4add8b7f4434b1
arismuslimin <93474156+arismuslimin@users.noreply.github.com>
2022-08-31 08:12:33 +0700

    Update cek-ss.sh

--- menu/cek/cek-ss.sh
+++ menu/cek/cek-ss.sh
@@ -28,10 +28,10 @@ domain=$(cat /etc/xray/domain)
 else
 domain=$IP
 fi
-tls="$(cat ~/log-install.txt | grep -w "VMESS WS TLS" | cut -d: -f2|sed 's/ //g')"
-nontls="$(cat ~/log-install.txt | grep -w "VMESS WS HTTP" | cut -d: -f2|sed 's/ //g')"
+tls="$(cat ~/log-install.txt | grep -w "HADOWSOCKS WS TLS" | cut -d: -f2|sed 's/ //g')"
+nontls="$(cat ~/log-install.txt | grep -w "SHADOWSOCKS WS HTTP" | cut -d: -f2|sed 's/ //g')"
 clear
-NUMBER_OF_CLIENTS=$(grep -c -E "^### " "/etc/xray/config.json")
+NUMBER_OF_CLIENTS=$(grep -c -E "^#### " "/etc/xray/config.json")
 	if [[ ${NUMBER_OF_CLIENTS} == '0' ]]; then
 		echo ""
 		echo "You have no existing clients!"
