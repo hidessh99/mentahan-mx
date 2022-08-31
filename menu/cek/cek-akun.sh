bd8955689e999dedb73740f3afbe8272d7e586e5
arismuslimin <93474156+arismuslimin@users.noreply.github.com>
2022-08-31 09:02:57 +0700

    Update cek-akun.sh

--- menu/cek/cek-akun.sh
+++ menu/cek/cek-akun.sh
@@ -32,9 +32,6 @@ tls="$(cat ~/log-install.txt | grep -w "VMESS WS TLS" | cut -d: -f2|sed 's/ //g'
 nontls="$(cat ~/log-install.txt | grep -w "VMESS WS HTTP" | cut -d: -f2|sed 's/ //g')"
 clear
 NUMBER_OF_CLIENTS=$(grep -c -E "^# " "/etc/xray/config.json")
-NUMBER_OF_CLIENTS=$(grep -c -E "^## " "/etc/xray/config.json")
-NUMBER_OF_CLIENTS=$(grep -c -E "^### " "/etc/xray/config.json")
-NUMBER_OF_CLIENTS=$(grep -c -E "^#### " "/etc/xray/config.json")
 	if [[ ${NUMBER_OF_CLIENTS} == '0' ]]; then
 		echo ""
 		echo "You have no existing clients!"
