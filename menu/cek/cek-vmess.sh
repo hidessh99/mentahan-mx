014646d09454902ea78c98e302d347911feb07c3
arismuslimin <93474156+arismuslimin@users.noreply.github.com>
2022-09-01 02:49:05 +0700

    Update cek-vmess.sh

--- menu/cek/cek-vmess.sh
+++ menu/cek/cek-vmess.sh
@@ -43,8 +43,8 @@ NUMBER_OF_CLIENTS=$(grep -c -E "^### " "/etc/xray/config.json")
 	echo " Select the existing client you want to remove"
 	echo " Press CTRL+C to return"
 	echo " ==============================="
-	echo -e	"${NC} NO ${GREEN}USER   ${RED}EXPIRED  ${BLUE}Net${NC}"
-        grep -E "^### " "/etc/xray/config.json" | cut -d ' ' -f 2,3,6,7 | nl -s ') '
+	echo -e	"${NC}	 NO ${GREEN}USER   ${RED}EXPIRED  ${BLUE}Net${NC}"
+        grep -E "^### " "/etc/xray/config.json" | cut -d ' ' -f ${GREEN}2,${RED}3,${BLUE}6,${BLUE}7 | nl -s ') '
 	until [[ ${CLIENT_NUMBER} -ge 1 && ${CLIENT_NUMBER} -le ${NUMBER_OF_CLIENTS} ]]; do
 		if [[ ${CLIENT_NUMBER} == '1' ]]; then
 			read -rp "Select one client [1]: " CLIENT_NUMBER
