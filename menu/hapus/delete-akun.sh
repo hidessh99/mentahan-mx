42f5e3aa112755f75f7dc20e881c4e6e66f2741b
arismuslimin <93474156+arismuslimin@users.noreply.github.com>
2022-08-31 10:45:07 +0700

    Update delete-akun.sh

--- menu/hapus/delete-akun.sh
+++ menu/hapus/delete-akun.sh
@@ -38,7 +38,10 @@ NUMBER_OF_CLIENTS=$(grep -c -E "^#### " "/etc/xray/config.json")
 	echo " Press CTRL+C to return"
 	echo " ==============================="
 	echo "     No    User  Expired"
-	grep -E "^#-^# " "/etc/xray/config.json" | cut -d ' ' -f 2-3 | nl -s ') '
+        grep -E "^# " "/etc/xray/config.json" | cut -d ' ' -f 2-3 | nl -s ') '
+	grep -E "^## " "/etc/xray/config.json" | cut -d ' ' -f 2-3 | nl -s ') '
+	grep -E "^### " "/etc/xray/config.json" | cut -d ' ' -f 2-3 | nl -s ') '
+	grep -E "^#### " "/etc/xray/config.json" | cut -d ' ' -f 2-3 | nl -s ') '
 	until [[ ${CLIENT_NUMBER} -ge 1 && ${CLIENT_NUMBER} -le ${NUMBER_OF_CLIENTS} ]]; do
 		if [[ ${CLIENT_NUMBER} == '1' ]]; then
 			read -rp "Select one client [1]: " CLIENT_NUMBER
