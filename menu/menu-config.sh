388f65251b3d27f4de81e2d39076ffcb4538ace6
arismuslimin <93474156+arismuslimin@users.noreply.github.com>
2022-09-01 12:10:45 +0700

    Update menu-config.sh

--- menu/menu-config.sh
+++ menu/menu-config.sh
@@ -1,23 +1,102 @@
 #!/bin/bash
+# ==========================================
+# Color
+RED='\033[0;31m'
+NC='\033[0m'
+GREEN='\033[0;32m'
+ORANGE='\033[0;33m'
+BLUE='\033[0;34m'
+PURPLE='\033[0;35m'
+CYAN='\033[0;36m'
+LIGHT='\033[0;37m'
+# ==========================================
+# Getting
 clear
-echo -e "\e[36m╒════════════════════════════════════════════╕\033[0m"
-echo -e " \E[0;41;36m                 INFO SERVER                \E[0m"
-echo -e "\e[36m╘════════════════════════════════════════════╛\033[0m"
-uphours=`uptime -p | awk '{print $2,$3}' | cut -d , -f1`
-upminutes=`uptime -p | awk '{print $4,$5}' | cut -d , -f1`
-uptimecek=`uptime -p | awk '{print $6,$7}' | cut -d , -f1`
-cekup=`uptime -p | grep -ow "day"`
-IPVPS=$(curl -s ipinfo.io/ip )
-ISPVPS=$( curl -s ipinfo.io/org )
-#clear
-if [ "$cekup" = "day" ]; then
-echo -e "System Uptime   :  $uphours $upminutes $uptimecek"
+IP=$(wget -qO- ipinfo.io/ip);
+date=$(date +"%Y-%m-%d");
+Green_font_prefix="\033[32m" && Red_font_prefix="\033[31m" && Green_background_prefix="\033[42;37m" && Red_background_prefix="\033[41;37m" && Font_color_suffix="\033[0m"
+Info="${Green_font_prefix}[ON]${Font_color_suffix}"
+Error="${Red_font_prefix}[OFF]${Font_color_suffix}"
+cek=$(grep -c -E "^# BEGIN_Backup" /etc/crontab)
+if [[ "$cek" = "1" ]]; then
+sts="${Info}"
 else
-echo -e "System Uptime   :  $uphours $upminutes"
+sts="${Error}"
 fi
-echo -e "IP-VPS          :  $IPVPS"
-echo -e "ISP-VPS         :  $ISPVPS"
-
+function start() {
+email=$(cat /home/email)
+if [[ "$email" = "" ]]; then
+echo "Please enter your email"
+read -rp "Email : " -e email
+cat <<EOF>>/home/email
+$email
+EOF
+fi
+cat << EOF >> /etc/crontab
+# BEGIN_Backup
+5 0 * * * root backup
+# END_Backup
+EOF
+service cron restart
+sleep 1
+echo " Please Wait"
+clear
+echo " Autobackup Has Been Started"
+echo " Data Will Be Backed Up Automatically at 00:05 GMT +7"
+exit 0
+}
+function stop() {
+email=$(cat /home/email)
+sed -i "/^$email/d" /home/email
+sed -i "/^# BEGIN_Backup/,/^# END_Backup/d" /etc/crontab
+service cron restart
+sleep 1
+echo " Please Wait"
+clear
+echo " Autobackup Has Been Stopped"
+exit 0
+}
+function gantipenerima() {
+rm -rf /home/email
+echo "Please enter your email"
+read -rp "Email : " -e email
+cat <<EOF>>/home/email
+$email
+EOF
+}
+function gantipengirim() {
+echo "Please enter your email"
+read -rp "Email : " -e email
+echo "Please enter your Password email"
+read -rp "Password : " -e pwdd
+rm -rf /etc/msmtprc
+cat<<EOF>>/etc/msmtprc
+defaults
+tls on
+tls_starttls on
+tls_trust_file /etc/ssl/certs/ca-certificates.crt
+account default
+host smtp.gmail.com
+port 587
+auth on
+user $email
+from $email
+password $pwdd
+logfile ~/.msmtp.log
+EOF
+}
+function testemail() {
+email=$(cat /home/email)
+if [[ "$email" = "" ]]; then
+start
+fi
+email=$(cat /home/email)
+echo -e "
+Ini adalah isi email percobaaan kirim email dari vps
+IP VPS : $IP
+Tanggal : $date
+" | mail -s "Percobaan Pengiriman Email" $email
+}
 function bugws() {
 rm -rf /etc/xray/bugws
 echo "Please enter your bugws"
