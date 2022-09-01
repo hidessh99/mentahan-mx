077ba0639b64c23fbf2121c843c1618482ee3f56
arismuslimin <93474156+arismuslimin@users.noreply.github.com>
2022-09-01 12:37:59 +0700

    Update delete-akun.sh

--- menu/hapus/delete-akun.sh
+++ menu/hapus/delete-akun.sh
@@ -23,9 +23,6 @@ exit 0
 fi
 clear
 NUMBER_OF_CLIENTS=$(grep -c -E "^# " "/etc/xray/config.json")
-NUMBER_OF_CLIENTS=$(grep -c -E "^## " "/etc/xray/config.json")
-NUMBER_OF_CLIENTS=$(grep -c -E "^### " "/etc/xray/config.json")
-NUMBER_OF_CLIENTS=$(grep -c -E "^#### " "/etc/xray/config.json")
 	if [[ ${NUMBER_OF_CLIENTS} == '0' ]]; then
 		echo ""
 		echo "You have no existing clients!"
