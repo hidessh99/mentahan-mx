095db38263f9ffe06702254ebac988f022367085
arismuslimin <93474156+arismuslimin@users.noreply.github.com>
2022-09-01 11:36:37 +0700

    Update add-configws.sh

--- menu/cek/add-configws.sh
+++ menu/cek/add-configws.sh
@@ -30,6 +30,8 @@ domain=$IP
 fi
 tls="$(cat ~/log-install.txt | grep -w "TROJAN WS TLS" | cut -d: -f2|sed 's/ //g')"
 nontls="$(cat ~/log-install.txt | grep -w "TROJAN WS HTTP" | cut -d: -f2|sed 's/ //g')"
+bugws="$(cat /etc/xray/bugws)"
+bugsni="$(cat /etc/xray/bugsni)"
 clear
 NUMBER_OF_CLIENTS=$(grep -c -E "^# " "/etc/xray/config.json")
 	if [[ ${NUMBER_OF_CLIENTS} == '0' ]]; then
@@ -53,16 +55,325 @@ NUMBER_OF_CLIENTS=$(grep -c -E "^# " "/etc/xray/config.json")
 		fi
 	done
 user=$(grep -E "^# " "/etc/xray/config.json" | cut -d ' ' -f 2 | sed -n "${CLIENT_NUMBER}"p)
+user=$(grep -E "^## " "/etc/xray/config.json" | cut -d ' ' -f 2 | sed -n "${CLIENT_NUMBER}"p)
+user=$(grep -E "^### " "/etc/xray/config.json" | cut -d ' ' -f 2 | sed -n "${CLIENT_NUMBER}"p)
+user=$(grep -E "^#### " "/etc/xray/config.json" | cut -d ' ' -f 2 | sed -n "${CLIENT_NUMBER}"p)
 exp=$(grep -E "^# " "/etc/xray/config.json" | cut -d ' ' -f 3 | sed -n "${CLIENT_NUMBER}"p)
+exp=$(grep -E "^## " "/etc/xray/config.json" | cut -d ' ' -f 3 | sed -n "${CLIENT_NUMBER}"p)
+exp=$(grep -E "^### " "/etc/xray/config.json" | cut -d ' ' -f 3 | sed -n "${CLIENT_NUMBER}"p)
+exp=$(grep -E "^#### " "/etc/xray/config.json" | cut -d ' ' -f 3 | sed -n "${CLIENT_NUMBER}"p)
 hariini=$(grep -E "^# " "/etc/xray/config.json" | cut -d ' ' -f 4 | sed -n "${CLIENT_NUMBER}"p)
+hariini=$(grep -E "^## " "/etc/xray/config.json" | cut -d ' ' -f 4 | sed -n "${CLIENT_NUMBER}"p)
+hariini=$(grep -E "^### " "/etc/xray/config.json" | cut -d ' ' -f 4 | sed -n "${CLIENT_NUMBER}"p)
+hariini=$(grep -E "^#### " "/etc/xray/config.json" | cut -d ' ' -f 4 | sed -n "${CLIENT_NUMBER}"p)
 uuid=$(grep -E "^# " "/etc/xray/config.json" | cut -d ' ' -f 5 | sed -n "${CLIENT_NUMBER}"p)
+uuid=$(grep -E "^## " "/etc/xray/config.json" | cut -d ' ' -f 5 | sed -n "${CLIENT_NUMBER}"p)
+uuid=$(grep -E "^### " "/etc/xray/config.json" | cut -d ' ' -f 5 | sed -n "${CLIENT_NUMBER}"p)
+uuid=$(grep -E "^#### " "/etc/xray/config.json" | cut -d ' ' -f 5 | sed -n "${CLIENT_NUMBER}"p)
+
+clear
 
 #buatlinktrojan
-trojanlinkwstls="trojan://${uuid}@${domain}:443?path=/xraytrojanws&security=tls&host=${domain}&type=ws&sni=${domain}#${user}"
-trojanlinkgrpc="trojan://${uuid}@${domain}:443?mode=gun&security=tls&type=grpc&serviceName=trojan-grpc&sni=${domain}#${user}"
+trojanlinkwstls="trojan://${uuid}@${bugws}:443?path=/xraytrojanws&security=tls&host=${domain}&type=ws&sni=${domain}#${user}"
+trojanlinkgrpc="trojan://${uuid}@${bugws}:443?mode=gun&security=tls&type=grpc&serviceName=trojan-grpc&sni=${domain}#${user}"
+
+#buatlinkvless
+vlesslinkws="vless://${uuid}@${bugws}:80?path=/xrayws&security=none&encryption=none&host=${domain}&type=ws#${user}"
+vlesslinkwstls="vless://${uuid}@${bugws}:443?path=/xrayws&security=tls&encryption=none&host=${domain}&type=ws&sni=${domain}#${user}"
+vlesslinkgrpc="vless://${uuid}@${bugws}:443?mode=gun&security=tls&encryption=none&type=grpc&serviceName=vless-grpc&sni=${domain}#${user}"
+
+#buatlinkvmess
+cat>/etc/xray/configws-vmess-$user-ws.json<<EOF
+      {
+      "v": "2",
+      "ps": "${user}",
+      "add": "${bugws}",
+      "port": "${nontls}",
+      "id": "${uuid}",
+      "aid": "0",
+      "net": "ws",
+      "path": "/xrayvws",
+      "type": "none",
+      "host": "${domain}",
+      "tls": ""
+}
+EOF
+cat>/etc/xray/configws-vmess-$user-wstls.json<<EOF
+      {
+      "v": "2",
+      "ps": "${user}",
+      "add": "${bugws}",
+      "port": "${tls}",
+      "id": "${uuid}",
+      "aid": "0",
+      "net": "ws",
+      "path": "/xrayvws",
+      "type": "none",
+      "host": "${domain}",
+      "tls": "tls"
+}
+EOF
+cat>/etc/xray/configws-vmess-$user-grpc.json<<EOF
+      {
+      "v": "2",
+      "ps": "${user}",
+      "add": "${bugws}",
+      "port": "${tls}",
+      "id": "${uuid}",
+      "aid": "0",
+      "net": "grpc",
+      "path": "vmess-grpc",
+      "type": "none",
+      "host": "${domain}",
+      "tls": "tls"
+}
+EOF
+cd
+clear
+vmess_base641=$( base64 -w 0 <<< $vmess_json1)
+vmess_base642=$( base64 -w 0 <<< $vmess_json2)
+vmesslinkws="vmess://$(base64 -w 0 /etc/xray/configws-vmess-$user-ws.json)"
+vmesslinkwstls="vmess://$(base64 -w 0 /etc/xray/configws-vmess-$user-wstls.json)"
+vmesslinkgrpc="vmess://$(base64 -w 0 /etc/xray/configws-vmess-$user-grpc.json)"
+
+#buatlinkshadowsocks
+echo $cipher:$uuid > /tmp/log
+shadowsocks_base64=$(cat /tmp/log)
+echo -n "${shadowsocks_base64}" | base64 > /tmp/log1
+shadowsocks_base64e=$(cat /tmp/log1)
+shadowsockslink="ss://${shadowsocks_base64e}@$bugws:$tls?plugin=xray-plugin;mux=0;path=/xrayssws;host=$domain;tls#${user}"
+shadowsockslinkgrpc="ss://${shadowsocks_base64e}@$bugws:$tls?plugin=xray-plugin;mux=0;serviceName=ss-grpc;host=$domain;tls#${user}"
+#buatlinkconfigtextshadowshock
+cat > /home/vps/public_html/configws-ss-ws-$user.txt<<EOF
+{ 
+ "dns": {
+    "servers": [
+      "8.8.8.8",
+      "8.8.4.4"
+    ]
+  },
+ "inbounds": [
+   {
+      "port": 10808,
+      "protocol": "socks",
+      "settings": {
+        "auth": "noauth",
+        "udp": true,
+        "userLevel": 8
+      },
+      "sniffing": {
+        "destOverride": [
+          "http",
+          "tls"
+        ],
+        "enabled": true
+      },
+      "tag": "socks"
+    },
+    {
+      "port": 10809,
+      "protocol": "http",
+      "settings": {
+        "userLevel": 8
+      },
+      "tag": "http"
+    }
+  ],
+  "log": {
+    "loglevel": "none"
+  },
+  "outbounds": [
+    {
+      "mux": {
+        "enabled": true
+      },
+      "protocol": "shadowsocks",
+      "settings": {
+        "servers": [
+          {
+            "address": "$bugws",
+            "level": 8,
+            "method": "$cipher",
+            "password": "$uuid",
+            "port": 443
+          }
+        ]
+      },
+      "streamSettings": {
+        "network": "ws",
+        "security": "tls",
+        "tlsSettings": {
+          "allowInsecure": true,
+          "serverName": "$domain"
+        },
+        "wsSettings": {
+          "headers": {
+            "Host": "$domain"
+          },
+          "path": "/xrayssws"
+        }
+      },
+      "tag": "proxy"
+    },
+    {
+      "protocol": "freedom",
+      "settings": {},
+      "tag": "direct"
+    },
+    {
+      "protocol": "blackhole",
+      "settings": {
+        "response": {
+          "type": "http"
+        }
+      },
+      "tag": "block"
+    }
+  ],
+  "policy": {
+    "levels": {
+      "8": {
+        "connIdle": 300,
+        "downlinkOnly": 1,
+        "handshake": 4,
+        "uplinkOnly": 1
+      }
+    },
+    "system": {
+      "statsOutboundUplink": true,
+      "statsOutboundDownlink": true
+    }
+  },
+  "routing": {
+    "domainStrategy": "Asls",
+"rules": []
+  },
+  "stats": {}
+}
+EOF
+cat > /home/vps/public_html/configws-ss-grpc-$user.txt<<EOF
+{
+    "dns": {
+    "servers": [
+      "8.8.8.8",
+      "8.8.4.4"
+    ]
+  },
+ "inbounds": [
+   {
+      "port": 10808,
+      "protocol": "socks",
+      "settings": {
+        "auth": "noauth",
+        "udp": true,
+        "userLevel": 8
+      },
+      "sniffing": {
+        "destOverride": [
+          "http",
+          "tls"
+        ],
+        "enabled": true
+      },
+      "tag": "socks"
+    },
+    {
+      "port": 10809,
+      "protocol": "http",
+      "settings": {
+        "userLevel": 8
+      },
+      "tag": "http"
+    }
+  ],
+  "log": {
+    "loglevel": "none"
+  },
+  "outbounds": [
+    {
+      "mux": {
+        "enabled": true
+      },
+      "protocol": "shadowsocks",
+      "settings": {
+        "servers": [
+          {
+            "address": "$bugws",
+            "level": 8,
+            "method": "$cipher",
+            "password": "$uuid",
+            "port": 443
+          }
+        ]
+      },
+      "streamSettings": {
+        "grpcSettings": {
+          "multiMode": true,
+          "serviceName": "ss-grpc"
+        },
+        "network": "grpc",
+        "security": "tls",
+        "tlsSettings": {
+          "allowInsecure": true,
+          "serverName": "$domain"
+        }
+      },
+      "tag": "proxy"
+    },
+    {
+      "protocol": "freedom",
+      "settings": {},
+      "tag": "direct"
+    },
+    {
+      "protocol": "blackhole",
+      "settings": {
+        "response": {
+          "type": "http"
+        }
+      },
+      "tag": "block"
+    }
+  ],
+  "policy": {
+    "levels": {
+      "8": {
+        "connIdle": 300,
+        "downlinkOnly": 1,
+        "handshake": 4,
+        "uplinkOnly": 1
+      }
+    },
+    "system": {
+      "statsOutboundUplink": true,
+      "statsOutboundDownlink": true
+    }
+  },
+  "routing": {
+    "domainStrategy": "Asls",
+"rules": []
+  },
+  "stats": {}
+}
+EOF
+
+clear
+#buat text all link
+cat > /home/vps/public_html/configws-multiakun-$user.txt<<EOF
+${trojanlinkwstls}
+${trojanlinkgrpc}
+${vlesslinkws}
+${vlesslinkwstls}
+${vlesslinkgrpc}
+${vmesslinkws}
+${vmesslinkwstls}
+${vmesslinkgrpc}
+EOF
+cd
+clear
 clear
 echo -e ""
-echo -e "========-TROJAN-GO-======="
+echo -e "======-VMESS/VLESS/TROJAN-GO/SHADOWSHOCK-======"
 echo -e "Remarks     : ${user}"
 echo -e "IP/Host     : ${MYIP}"
 echo -e "Address     : ${domain}"
@@ -72,17 +383,53 @@ echo -e "User ID     : ${uuid}"
 echo -e "Alter ID    : 0"
 echo -e "Security    : auto"
 echo -e "Network     : ws/grpc"
-echo -e "Path        : /xraytrojanws/trojan-grpc"
+echo -e "Path ws     : /xrayvws/xrayws/xraytrojanws"
+echo -e "Path grpc   : /vmess-grpc/vless-grpc/trojan-grpc"
 echo -e "Created     : $hariini"
 echo -e "Expired     : $exp"
 echo -e "========================="
-echo -e "link   trojan ws"
-echo -e "${NC}${GREEN} ${trojanlinkws} ${NC}"
+echo -e "link   vmess ws"
+echo -e
+echo -e "${NC}${GREEN} ${vmesslinkws} ${NC}"
+echo -e
 echo -e "========================="
-echo -e "link   trojan ws tls"
+echo -e "link   vmess ws tls"
+echo -e
+echo -e "${NC}${ORANGE} ${vmesslinkwstls} ${NC}"
+echo -e
+echo -e "========================="
+echo -e "link vmess grpc"
+echo -e
+echo -e "${NC}${BLUE} ${vmesslinkgrpc} ${NC}"
+echo -e
+echo -e "========================="
+echo -e "link vless WS"
+echo -e "${NC}${GREEN} ${vlesslinkws} ${NC}"
+echo -e "========================="
+echo -e "link vless WS tls"
+echo -e "${NC}${ORANGE} ${vlesslinkwstls} ${NC}"
+echo -e "========================="
+echo -e "link vless grpc"
+echo -e "${NC}${BLUE}${vlesslinkgrpc} ${NC}"
+echo -e "========================="
+echo -e "link trojan WS tls"
 echo -e "${NC}${ORANGE} ${trojanlinkwstls} ${NC}"
 echo -e "========================="
 echo -e "link trojan grpc"
 echo -e "${NC}${BLUE} ${trojanlinkgrpc} ${NC}"
 echo -e "========================="
-echo -e "AKCELL XRAY MULTI TROJAN-GO"
+echo -e "link shadowshock"
+echo -e "${NC}${ORANGE} ${shadowsockslink} ${NC}"
+echo -e "========================="
+echo -e "link shadowshock grpc"
+echo -e "${NC}${BLUE} ${shadowsockslinkgrpc} ${NC}"
+echo -e "========================="
+echo -e "======Custom Import Config From URL ======="
+echo -e "URL Custom Config WS TLS   : ${NC}${ORANGE} http://${domain}:89/configws-ss-ws-$user.txt ${NC}" 
+echo -e "URL Custom Config GRPC TLS : ${NC}${BLUE} http://${domain}:89/configws-ss-grpc-$user.txt ${NC}" 
+echo -e "===================================================="
+echo -e "======Custom Import Config From URL ======="
+echo -e "URL CONFIG V2RAYNG all trojan-vless-vmess : ${NC}${RED} http://${domain}:89/configws-multiakun-$user.txt ${NC}" 
+echo -e "===================================================="
+echo -e                "AKCELL XRAY MULTI AKUN"
+echo -e "===================================================="
