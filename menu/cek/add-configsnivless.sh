090ec89d2ca384feaa3eda1c28471428d4634dd0
arismuslimin <93474156+arismuslimin@users.noreply.github.com>
2022-09-01 16:02:58 +0700

    Create add-configsnivless.sh

---
+++ menu/cek/add-configsnivless.sh
@@ -0,0 +1,103 @@
+#!/bin/bash
+# ==========================================
+# Color
+RED='\033[0;31m'
+NC='\033[0m'
+GREEN='\033[0;32m'
+ORANGE='\033[0;33m'
+BLUE='\033[0;34m'
+PURPLE='\033[0;35m'
+CYAN='\033[0;36m'
+LIGHT='\033[0;37m'
+# ==========================================
+# Getting
+MYIP=$(wget -qO- ipinfo.io/ip);
+echo "Checking VPS"
+IZIN=$( curl ipinfo.io/ip | grep $MYIP )
+if [ $MYIP = $MYIP ]; then
+echo -e "${NC}${GREEN}Permission Accepted...${NC}"
+else
+echo -e "${NC}${RED}Permission Denied!${NC}";
+echo -e "${NC}${LIGHT}Fuck You!!"
+exit 0
+fi
+clear
+source /var/lib/crot/ipvps.conf
+if [[ "$IP" = "" ]]; then
+domain=$(cat /etc/xray/domain)
+else
+domain=$IP
+fi
+tls="$(cat ~/log-install.txt | grep -w "TROJAN WS TLS" | cut -d: -f2|sed 's/ //g')"
+nontls="$(cat ~/log-install.txt | grep -w "TROJAN WS HTTP" | cut -d: -f2|sed 's/ //g')"
+bugws="$(cat /etc/xray/bugws)"
+bugsni="$(cat /etc/xray/bugsni)"
+clear
+NUMBER_OF_CLIENTS=$(grep -c -E "^## " "/etc/xray/config.json")
+	if [[ ${NUMBER_OF_CLIENTS} == '0' ]]; then
+		echo ""
+		echo "You have no existing clients!"
+		exit 1
+	fi
+
+	clear
+	echo ""
+	echo " Select the existing client you want to remove"
+	echo " Press CTRL+C to return"
+	echo " ==============================="
+	echo -e	"  NO ${GREEN}USER   ${RED}EXPIRED ${BLUE}Net${NC}"
+        grep -E "^## " "/etc/xray/config.json" | cut -d ' ' -f 2,3,6,7 | nl -s ') '
+	until [[ ${CLIENT_NUMBER} -ge 1 && ${CLIENT_NUMBER} -le ${NUMBER_OF_CLIENTS} ]]; do
+		if [[ ${CLIENT_NUMBER} == '1' ]]; then
+			read -rp "Select one client [1]: " CLIENT_NUMBER
+		else
+			read -rp "Select one client [1-${NUMBER_OF_CLIENTS}]: " CLIENT_NUMBER
+		fi
+	done
+user=$(grep -E "^## " "/etc/xray/config.json" | cut -d ' ' -f 2 | sed -n "${CLIENT_NUMBER}"p)
+exp=$(grep -E "^## " "/etc/xray/config.json" | cut -d ' ' -f 3 | sed -n "${CLIENT_NUMBER}"p)
+hariini=$(grep -E "^## " "/etc/xray/config.json" | cut -d ' ' -f 4 | sed -n "${CLIENT_NUMBER}"p)
+uuid=$(grep -E "^## " "/etc/xray/config.json" | cut -d ' ' -f 5 | sed -n "${CLIENT_NUMBER}"p)
+
+clear
+#buatlinktrojan
+trojanlinkwstls="trojan://${uuid}@${domain}:443?path=/xraytrojanws&security=tls&host=${domain}&type=ws&sni=${bugsni}#${user}"
+trojanlinkgrpc="trojan://${uuid}@${domain}:443?mode=gun&security=tls&type=grpc&serviceName=trojan-grpc&sni=${bugsni}#${user}"
+#buat text all link
+cat > /home/vps/public_html/configsni-vless-$user.txt<<EOF
+${vlesslinkws}
+${vlesslinkwstls}
+${vlesslinkgrpc}
+EOF
+cd
+clear
+echo -e ""
+echo -e "======-VLESS-======"
+echo -e "Remarks     : ${user}"
+echo -e "IP/Host     : ${MYIP}"
+echo -e "Address     : ${domain}"
+echo -e "Port TLS    : ${tls}"
+echo -e "Port No TLS : ${nontls}"
+echo -e "User ID     : ${uuid}"
+echo -e "Alter ID    : 0"
+echo -e "Security    : auto"
+echo -e "Network     : ws/grpc"
+echo -e "Path ws     : /xrayws"
+echo -e "Path grpc   : vless-grpc"
+echo -e "Created     : $hariini"
+echo -e "Expired     : $exp"
+echo -e "========================="
+echo -e "link vless WS"
+echo -e "${NC}${GREEN} ${vlesslinkws} ${NC}"
+echo -e "========================="
+echo -e "link vless WS tls"
+echo -e "${NC}${ORANGE} ${vlesslinkwstls} ${NC}"
+echo -e "========================="
+echo -e "link vless grpc"
+echo -e "${NC}${BLUE}${vlesslinkgrpc} ${NC}"
+echo -e "========================="
+echo -e "======Custom Import Config From URL ======="
+echo -e "URL CONFIG V2RAYNG vless : ${NC}${RED} http://${domain}:89/configsni-vless-$user.txt ${NC}" 
+echo -e "===================================================="
+echo -e                "AKCELL XRAY MULTI VLESS"
+echo -e "===================================================="
